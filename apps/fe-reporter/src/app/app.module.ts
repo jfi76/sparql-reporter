4f60faaa2ae8cb2d5d1e80a27ae9b09e1997cc58
jfi76 <eugene.philipov@gmail.com>
2022-12-29 19:30:39 +0200

    splitter

--- apps/fe-reporter/src/app/app.module.ts
+++ apps/fe-reporter/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularSplitModule } from 'angular-split';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgbModule, AngularSplitModule,],
   providers: [],
   bootstrap: [AppComponent],
+  exports:[NxWelcomeComponent]
 })
 export class AppModule {}
